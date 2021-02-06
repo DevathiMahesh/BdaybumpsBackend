02ed653d52e3b44123c3adf738b280d73d1caefc
DevathiMahesh <d.mahesh995@gmail.com>
2021-02-06 18:58:03 +0530

    firstcommit

---
+++ src/main/java/com/bdaybumps/demo/BdaybumpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bdaybumps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdaybumpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdaybumpsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdaybumps/demo/BdaybumpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdaybumps.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdaybumpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
