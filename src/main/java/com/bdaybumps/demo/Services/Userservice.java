ddfcc61f222252e0805f9210fecab273eb526396
DevathiMahesh <d.mahesh995@gmail.com>
2021-02-11 21:27:30 +0530

    Logger added to the project.

--- src/main/java/com/bdaybumps/demo/Services/Userservice.java
+++ src/main/java/com/bdaybumps/demo/Services/Userservice.java
@@ -3,6 +3,9 @@ package com.bdaybumps.demo.Services;
 import com.bdaybumps.demo.Domains.UserEntity;
 import com.bdaybumps.demo.Models.UserModelDTO;
 import com.bdaybumps.demo.Repository.UserRepository;
+import com.bdaybumps.demo.controllers.LoggingController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ public class Userservice {
     private UserRepository userRepository;
     @Autowired
     private GmailService gmailService;
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
     public  ResponseEntity<UserEntity> createUser(UserModelDTO user)
     {
         UserEntity userEntity = new UserEntity(
@@ -29,6 +33,7 @@ public class Userservice {
         );
         System.out.println(user);
         gmailService.sendEmail(user.getFirstName());
+        logger.info("New User Created.An Email is sent.");
         return new ResponseEntity<>(userRepository.save(userEntity), HttpStatus.CREATED);
     }
     public List<UserEntity> getAllUsers()
