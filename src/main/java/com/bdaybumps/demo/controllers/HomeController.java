5bb22b6024cdb738ce95487c5467294888804e2e
DevathiMahesh <d.mahesh995@gmail.com>
2021-02-17 22:53:43 +0530

    Cors added at method level

--- src/main/java/com/bdaybumps/demo/controllers/HomeController.java
+++ src/main/java/com/bdaybumps/demo/controllers/HomeController.java
@@ -14,10 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,6 +45,7 @@ public class HomeController {
     {
         return userservice.getAllUsers();
     }
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthRequest temp) throws Exception {
        try{
